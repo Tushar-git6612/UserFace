88b2e0c5d69f6e0592c83c27ca368d8268b36d88
tushar <tush44644@gmail.com>
2023-09-26 14:17:52 +0530

    my editor check

---
+++ src/Components/Myeditor.jsx
@@ -0,0 +1,193 @@
+import React, { Component } from "react";
+
+export default class Myeditor extends Component {
+  constructor() {
+    super();
+    this.state = {
+      editorarea: "type here",
+    };
+    this.editorChanger = this.editorChanger.bind(this);
+    this.textUppercase = this.textUppercase.bind(this);
+    this.textLowercase = this.textLowercase.bind(this);
+    this.textClear = this.textClear.bind(this);
+    this.textCapitalize = this.textCapitalize.bind(this);
+    this.copyText = this.copyText.bind(this);
+    this.boldText = this.boldText.bind(this);
+  }
+
+  editorChanger(event) {
+    this.setState({
+      [event.target.name]: event.target.value,
+    });
+  }
+
+  textUppercase() {
+    let upperState = this.state.editorarea.toUpperCase();
+    this.setState({
+      editorarea: upperState,
+    });
+  }
+
+  textLowercase() {
+    let lowerState = this.state.editorarea.toLowerCase();
+    this.setState({
+      editorarea: lowerState,
+    });
+  }
+
+  textClear() {
+    this.setState({
+      editorarea: "",
+    });
+  }
+
+  textCapitalize() {
+    let newStrings = this.state.editorarea.split(" ");
+    let words = newStrings.map((word) => {
+      if (word.length === 0) {
+        return "";
+      }
+      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    });
+    // change to string
+    let data = words.join(" ");
+    this.setState({
+      editorarea: data,
+    });
+  }
+
+  copyText(event) {
+    const text = document.querySelector("#textarea");
+    console.log(text);
+    text.select();
+    text.setSelectionRange(0, 9999);
+    navigator.clipboard.writeText(text.value);
+  }
+
+  boldText() {
+    let textarea = document.querySelector("textarea");
+    textarea.style.fontWeight = "900";
+  }
+
+  sentenceCase() {
+    let textarea = document.querySelector("textarea");
+    textarea.style.textAlign = "justify";  
+  }
+
+  textCenter() {
+    let textarea = document.querySelector("textarea");
+    textarea.style.textAlign = "center";
+  }
+
+  textRight(){
+  let textarea = document.querySelector("textarea");
+  textarea.style.textAlign="right"
+  }
+
+  render() {
+    return (
+      <>
+        <div className="container text-center my-2">
+          <textarea
+            className="form-control"
+            id="textarea"
+            onChange={this.editorChanger}
+            rows="12"
+            name="editorarea"
+            value={this.state.editorarea}
+          ></textarea>
+          <div className="d-flex mt-3 btnBox">
+            <div className="d-inline me-2">
+              <button
+                type="button"
+                onClick={this.textUppercase}
+                className="btn btn-outline-info"
+              >
+                Uppercase
+              </button>
+            </div>
+            <div className="d-inline me-2">
+              <button
+                type="button"
+                onClick={this.textLowercase}
+                className="btn btn-outline-info"
+              >
+                Lowercase
+              </button>
+            </div>
+            <div className="d-inline me-2">
+              <button
+                type="button"
+                onClick={this.textClear}
+                className="btn btn-outline-info"
+              >
+                Clear All
+              </button>
+            </div>
+            <div className="d-inline me-2">
+              <button
+                type="button"
+                onClick={() => {
+                  this.textCapitalize();
+                }}
+                className="btn btn-outline-info"
+              >
+                Capitalize
+              </button>
+            </div>
+            <div className="d-inline me-2">
+              <button
+                id="copyBtn"
+                type="button"
+                onClick={this.copyText}
+                className="btn btn-outline-info"
+              >
+                Copy
+              </button>
+            </div>
+            <div className="d-inline me-2">
+              <button
+                id="copyBtn"
+                type="button"
+                onClick={this.boldText}
+                className="btn btn-outline-info"
+              >
+                Bold
+              </button>
+            </div>
+            <div className="d-inline me-2">
+              <button
+                id="copyBtn"
+                type="button"
+                onClick={this.sentenceCase}
+                className="btn btn-outline-info"
+              >
+                Sentence
+              </button>
+            </div>
+            <div className="d-inline me-2">
+              <button
+                id="copyBtn"
+                type="button"
+                onClick={this.textCenter}
+                className="btn btn-outline-info"
+              >
+                Align center
+              </button>
+            </div>
+            <div className="d-inline me-2">
+              <button
+                id="copyBtn"
+                type="button"
+                onClick={this.textRight}
+                className="btn btn-outline-info"
+              >
+                Align Right
+              </button>
+            </div>
+          </div>
+        </div>
+      </>
+    );
+  }
+}
---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,35 @@
+import React, { Component } from "react";
+
+export default class Navbar extends Component {
+  render() {
+    return (
+      <>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <div className="container-fluid">
+            <a className="navbar-brand" href="/">
+              Navbar
+            </a>
+            <button
+              className="navbar-toggler"
+              type="button"
+              data-bs-toggle="collapse"
+              data-bs-target="#navbarNavAltMarkup"
+              aria-controls="navbarNavAltMarkup"
+              aria-expanded="false"
+              aria-label="Toggle navigation"
+            >
+              <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div className="navbar-nav">
+                <a className="nav-link active" aria-current="page" href="/">
+                  Home
+                </a>
+              </div>
+            </div>
+          </div>
+        </nav>
+      </>
+    );
+  }
+}
