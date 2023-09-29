2d27d72313015426c529f198559687ce2fc0da55
tushar <tush44644@gmail.com>
2023-09-29 10:37:35 +0530

    comment line no5 in App.js

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import Myeditor from "./Components/Myeditor";
 import Navbar from "./Components/Navbar";
 import "./App.css";
-import Login from "./Components/Login";
+// import Login from "./Components/Login";
 
 export default class App extends Component {
   render() {
